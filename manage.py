fe016f73be7a376a7107eedcacba429a3c8f6d8a
mohammadmahdi <mm.ahmadi0101@gmail.com>
2021-11-16 15:22:44 +0330

    Change project name. Add app_user, app_signin, app_signup. add .gitignore file.

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backofquiz.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quiz.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
