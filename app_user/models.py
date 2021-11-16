95b49d22ea510769c320bee97b3821a1feb2f059
mohammadmahdi <mm.ahmadi0101@gmail.com>
2021-11-16 16:17:01 +0330

    add User model

--- app_user/models.py
+++ app_user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(max_length=50, null=False, blank=False, unique=True)
+    first_name = models.CharField(max_length=20, null=True, blank=True, unique=False)
+    last_name = models.CharField(max_length=20, null=True, blank=True, unique=False)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
--- quiz/settings.py
+++ quiz/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # "quizapp",
+    "app_user",
 ]
 
 MIDDLEWARE = [
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'app_user.User'
